e5c9bf30659bf94502c456bc80a268b3fb7dc833
Arpan Swaroop <arpan.swaroop@gmail.com>
2024-06-11 17:36:12 -0500

    blinky BSP

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,26 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
-use cortex_m::asm::nop;
-use cortex_m_rt::entry;
-use embedded_hal::digital::{OutputPin, PinState};
-use hal::pac;
-use nrf52833_hal::{
-    self as hal,
-    gpio::{p0::Parts, Level},
-};
-use nrf52833_pac::Peripherals;
 use panic_halt as _;
+
+use cortex_m_rt::entry;
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+use microbit::{board::Board, hal::timer::Timer};
 
 #[entry]
 fn main() -> ! {
-    let p: Peripherals = pac::Peripherals::take().unwrap();
-    let port0: Parts = hal::gpio::p0::Parts::new(p.P0);
-    let _col1 = port0.p0_28.into_push_pull_output(Level::Low);
-    let mut row1 = port0.p0_21.into_push_pull_output(Level::Low);
+    let mut board = Board::take().unwrap();
 
-    let mut is_on: bool = false;
+    let mut timer = Timer::new(board.TIMER0);
+
+    let _ = board.display_pins.col1.set_low();
+    let mut row1 = board.display_pins.row1;
+
     loop {
-        let _ = row1.set_state(PinState::from(is_on));
-
-        for _ in 0..200_000 {
-            nop();
-        }
-        is_on = !is_on;
+        let _ = row1.set_low();
+        timer.delay_ms(1_000);
+        let _ = row1.set_high();
+        timer.delay_ms(1_000);
     }
-    // let mut x: usize = 0;
-    // loop {
-    //     x += 1;
-    //     for _ in 0..x {
-    //         nop();
-    //     }
-    // }
 }
+
