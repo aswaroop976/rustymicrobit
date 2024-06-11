00da2417e9105c368f85f8de981d76eca209d7e5
Arpan Swaroop <arpan.swaroop@gmail.com>
2024-06-11 17:32:04 -0500

    HAL time

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,26 @@
 
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
+use embedded_hal::digital::{OutputPin, PinState};
+use hal::pac;
+use nrf52833_hal::{
+    self as hal,
+    gpio::{p0::Parts, Level},
+};
 use nrf52833_pac::Peripherals;
 use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
-    let p: Peripherals = Peripherals::take().unwrap();
-    p.P0.pin_cnf[21].write(|writable| writable.dir().output());
-    p.P0.pin_cnf[28].write(|writable| writable.dir().output());
+    let p: Peripherals = pac::Peripherals::take().unwrap();
+    let port0: Parts = hal::gpio::p0::Parts::new(p.P0);
+    let _col1 = port0.p0_28.into_push_pull_output(Level::Low);
+    let mut row1 = port0.p0_21.into_push_pull_output(Level::Low);
 
     let mut is_on: bool = false;
     loop {
-        p.P0.out.write(|writable| writable.pin21().bit(is_on));
+        let _ = row1.set_state(PinState::from(is_on));
+
         for _ in 0..200_000 {
             nop();
         }
